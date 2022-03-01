91d9d063d54784fd94604f001698e4ba8b27f8d4
dmitrii koshelev <delta0ll0lll@gmail.com>
2022-03-01 02:11:50 +0000

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  printWidth: 120
+};
